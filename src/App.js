00685d8fa87e4cae0ec7c7d22242ebedbe0296a9
Shardy678 <tambov.j678@gmail.com>
2021-11-24 10:58:01 +0300

    user now can delete tasks

--- src/App.js
+++ src/App.js
@@ -20,6 +20,10 @@ function App(props) {
     });
     setTasks(updatedTasks);
   }
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter((task) => id != task.id);
+    setTasks(remainingTasks);
+  }
   const taskList = tasks.map((task) => (
     <Todo
       id={task.id}
@@ -27,6 +31,7 @@ function App(props) {
       completed={task.completed}
       key={task.id}
       toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
     />
   ));
   const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
